4d43d7e163ea27c706eeea78a79c0b79d56487ca
Mandeep Kaur <mandeep.fullstack.dev@gmail.com>
2023-04-16 13:45:56 +0530

    done with ui part of products component

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,21 @@
-import "./Products.scss";
+import Product from './Product/Product';
+import './Products.scss';
 const Products = () => {
-    return <div>Products</div>;
+  return (
+    <div className="products-container">
+      <div className="sec-heading">Section Heading</div>
+      <div className="products">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    </div>
+  );
 };
 
 export default Products;
