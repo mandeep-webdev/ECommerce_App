d06bd8d5d0ec8108a479a8a340acc2aa05f9d3e9
Mandeep Kaur <mandeep.fullstack.dev@gmail.com>
2023-04-19 12:25:56 +0530

    fetching categories and products data via api

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,38 @@
+import { useEffect, useContext } from 'react';
+import { Context } from '../../utils/context';
+import { fetchDataFromApi } from '../../utils/api';
 import './Home.scss';
 import Banner from './Banner/Banner';
 import Category from './Category/Category';
 import Products from '../Products/Products';
 const Home = () => {
+  const { categories, setCategories, products, setProducts } =
+    useContext(Context);
+
+  useEffect(() => {
+    getCategories();
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    const res = await fetchDataFromApi('/api/products?populate=*');
+    console.log(res.data);
+    setProducts(res.data);
+  };
+
+  const getCategories = async () => {
+    const res = await fetchDataFromApi('/api/categories?populate=*');
+    console.log(res.data);
+    setCategories(res.data);
+  };
+
   return (
     <div>
       <Banner />
       <div className="main-content">
         <div className="layout">
-          <Category />
-          <Products headingText="Popular Products" />
+          <Category categories={categories} />
+          <Products headingText="Popular Products" products={products} />
         </div>
       </div>
     </div>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,6 @@
 import Product from './Product/Product';
 import './Products.scss';
-const Products = ({ headingText }) => {
+const Products = ({ headingText, products }) => {
   return (
     <div className="products-container">
       <div className="sec-heading">{headingText}</div>
